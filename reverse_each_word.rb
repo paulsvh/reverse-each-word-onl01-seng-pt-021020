84a77959d1c6feef2579d05048a9d0b2633f1228
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-14 03:51:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_to_array = string.split
   backwards_array = []
-  string_to_array.each do |backwards|
+  string_to_array.collect do |backwards|
     backwards_array << backwards.reverse
   end
   backwards_array.join(" ")
